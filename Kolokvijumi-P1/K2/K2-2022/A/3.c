e2c3c1e274c7b8cfb66e123c05414f1c56e04fd6
Nebojša Kaišarević <nebojsa.kaisarevic@student.etf.unibl.org>
2025-01-10 18:16:59 +0100

    K2-2022/A/3. zadatak

---
+++ Kolokvijumi-P1/K2/K2-2022/A/3.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int is_lower(char c){
+    if (c>=97 && c<=122) return 1;
+    else if (c>=65 && c<90) return 0;
+    else return -1;
+}
+void upper(char *s){
+    if (*s>=97 && *s<=122) *s=*s-32;
+}
+int string_lenght (const char *string){
+    int counter = 0;
+    for (int i = 0; string[i]!='\0'; i++){
+        counter++;
+    }
+    return counter;
+}
+
+void clear_new_line (char *string){
+    for (int i = 0; string[i]!='\0'; i++){
+        if (string[i]=='\n') string[i]='\0';
+    }
+}
+
+
+int main (int argc, char *argv[]){
+    char string[100];
+    printf ("Unesite string: ");
+    fgets(string, sizeof(string), stdin);
+    clear_new_line(string);
+
+    char modified_string[string_lenght(string)];
+    for (int i = 0; i<string_lenght(string); i++){
+            modified_string[i]=string[i];
+        if (is_lower(string[i])){
+            upper(&modified_string[i]);
+        }
+    }
+        modified_string[string_lenght(string)] = '\0'; 
+        printf ("%s", modified_string);
+
+}
